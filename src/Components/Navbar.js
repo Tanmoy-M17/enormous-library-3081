dae8b55f71e3d90bf526bbb7274c1d0dfd5e7a6c
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-07-23 11:39:30 +0530

    c1

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -27,8 +27,17 @@ const Navbar = () => {
                   <img width='30px' height='30px'
                   src={search_logo}  />
                 </div>
-                <input className={Styles.s1}   placeholder='india' />
-                <img width='30px' height='30px' src={arrow} />
+                <input className={Styles.s1}   />
+                
+                <select className={Styles.drp}>
+                  <option><h5>India</h5></option>
+                  <option><h5>Kerala</h5></option>
+                  <option><h5>Punjab</h5></option>
+                  {/* <option><h5>Maharastra</h5></option>
+                  <option><h5>Tamil Nadu</h5></option>
+                  <option><h5>Karnataka</h5></option> */}
+                </select>
+                {/* <img width='30px' height='30px' src={arrow} /> */}
              </div>
             <div className={Styles.inp2}>
               <input className={Styles.s2} placeholder='Find Cars, Mobile Phones and more...'/>
@@ -37,8 +46,14 @@ const Navbar = () => {
               </div>
             </div>
             <div className={Styles.last}>
-              <div><h5>ENGLISH</h5></div>
-            <div className={Styles.a}><img width='30px' height='30px' src={arrow} /></div>
+              <div>
+                <select className={Styles.drpd}>
+                  <option><h5>ENGLISH</h5></option>
+                  <option><h5>HINDI</h5></option>
+
+                </select>
+                </div>
+           
             <div className={Styles.b}><img width='30px' height='30px' src={msg} /></div>
             <div className={Styles.c}><img width='30px' height='30px' src={bell} /></div>
             <div className={Styles.d}><h5>LOGIN</h5></div>
